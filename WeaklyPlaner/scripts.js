834fefd5df60cb1cd1a495a10d29ee6515b1f50b
sabdulq1 <sabdulq@iu.edu>
2023-10-22 13:06:44 -0400

    sxs

--- WeaklyPlaner/scripts.js
+++ WeaklyPlaner/scripts.js
@@ -58,6 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     </table>
                     <button onclick="editPlan(${index})">Edit</button>
                     <button onclick="deletePlan(${index})">Delete</button>
+                    <button onclick="printPlan(${index})">Print</button>
                 `;
         plansContainer.appendChild(planElement);
       }
@@ -77,4 +78,60 @@ function deletePlan(index) {
 function editPlan(index) {
   localStorage.setItem("editIndex", index);
   location.href = "plans.html";
+}
+
+function printPlan(index) {
+  const plans = JSON.parse(localStorage.getItem("plans")) || [];
+  const plan = plans[index];
+  const printWindow = window.open("", "_blank");
+  printWindow.document.write(`
+    <html>
+      <head>
+        <title>${plan.title}</title>
+      </head>
+      <body>
+        <h1>${plan.title}</h1>
+        <table>
+          <thead>
+            <tr>
+              ${plan.week
+                .map((day) => `<th>${day.day}</th><th>Status</th>`)
+                .join("")}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              ${plan.week
+                .map(
+                  (day) => `
+                  <td data-label="${day.day}">
+                      ${day.tasks.map((task) => `<div>${task}</div>`).join("")}
+                  </td>
+                  <td data-label="Status">
+                      ${
+                        Array.isArray(day.status)
+                          ? day.status
+                              .map(
+                                (status) =>
+                                  `<div>${
+                                    status === "Done" ? "✅" : "❌"
+                                  }</div>`
+                              )
+                              .join("")
+                          : day.status === "Done"
+                          ? "✅"
+                          : "❌"
+                      }
+                  </td>
+              `
+                )
+                .join("")}
+            </tr>
+          </tbody>
+        </table>
+      </body>
+    </html>
+  `);
+  printWindow.document.close();
+  printWindow.print();
 }
